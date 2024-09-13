547d9521fb6d2c49907bf899a5c4aedc70b206b2
Frank Secilia <frank.secilia@gmail.com>
2024-09-13 04:57:33 -0400

    Add via support for gk61 pro ds.

---
+++ keyboards/skyloong/gk61/pro_ds/config.h
@@ -0,0 +1,18 @@
+// Copyright 2023 linlin012 (@linlin012)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define CAPS_LOCK_INDEX 28
+
+#define AW20216S_SDB_PIN A4
+#define AW20216S_CS_PIN_1 B12
+#define AW20216S_EN_PIN_1 A4
+
+#define LAYER_STATE_8BIT
+
+#define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
+#define ENABLE_COMPILE_KEYCODE
+
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
