df69dee7a8b22dc92efdf390be8bef884e449293
Frank Secilia <frank.secilia@gmail.com>
2024-11-04 05:18:08 -0500

    swoop: update default keymap

--- keyboards/bluebell/swoop/keymaps/via/keymap.c
+++ keyboards/bluebell/swoop/keymaps/via/keymap.c
@@ -35,6 +35,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
                      LT(NUM, KC_ESC), MO(NAV), OSM(MOD_LSFT),      QK_REP, KC_SPC,  LT(SYM, KC_ENT)
 ),
 
+[NAV] = LAYOUT_split_3x5_3(
+    KC_F10,  KC_F11,        KC_F12,        KC_TAB,        S(KC_TAB),         KC_PGUP, C(KC_LEFT), KC_UP,   C(KC_RGHT), KC_PGDN,
+    C(KC_A), OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT), KC_F3,             KC_HOME, KC_LEFT,    KC_DOWN, KC_RGHT,    KC_END,
+    C(KC_Z), C(KC_X),       C(KC_C),       C(KC_V),       KC_LGUI,           KC_DEL,  C(KC_V),    C(KC_C), C(KC_X),    C(KC_Z),
+                            KC_TRNS,       KC_TRNS,       KC_TRNS,           KC_TRNS, C(KC_BSPC), KC_BSPC
+),
+
 [NUM] = LAYOUT_split_3x5_3(
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TAB,  S(KC_6),          KC_COMM,   KC_7, KC_8,    KC_9,   S(KC_8),
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_EQL,           S(KC_EQL), KC_1, KC_2,    KC_3,   KC_MINS,
@@ -54,13 +61,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT), KC_NO,          KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_NO,
     KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_NO,          KC_F10,  KC_F4,   KC_F5,   KC_F6,   KC_NO,
                                   KC_TRNS,       KC_TRNS,       KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS
-),
-
-[NAV] = LAYOUT_split_3x5_3(
-    KC_F10,  KC_F11,        KC_F12,        KC_TAB,        S(KC_TAB),         KC_PGUP, C(KC_LEFT), KC_UP,   C(KC_RGHT), KC_PGDN,
-    C(KC_A), OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT), KC_F3,             KC_HOME, KC_LEFT,    KC_DOWN, KC_RGHT,    KC_END,
-    C(KC_Z), C(KC_X),       C(KC_C),       C(KC_V),       KC_LGUI,           KC_DEL,  C(KC_V),    C(KC_C), C(KC_X),    C(KC_Z),
-                            KC_TRNS,       KC_TRNS,       KC_TRNS,           KC_TRNS, C(KC_BSPC), KC_BSPC
 ),
 
 };
