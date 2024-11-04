f506fb3af704e6aa05d09148200c89cd02308b87
Frank Secilia <frank.secilia@gmail.com>
2024-11-04 10:57:14 -0500

    zoom65-v3: start roughing in config
    Mine is shipped, but not here yet, so I can't
    start testing yet. The most recent via json I can
    find has a new vendor and product id, which makes
    me wary to just start flashing without seeing the
    new qmk .json. It may be ok, though, because the
    ids changed between v1 and the lite version, but
    the other bit configs stayed the same.
    The via .json suggest support for
    rgb_matrix, but I can't find pin information yet,
    so there is at least one hardware difference.

---
+++ keyboards/meletrix/zoom65-v3/keymaps/via/rules.mk
@@ -0,0 +1,12 @@
+VIA_ENABLE = yes
+
+# encoder map uses eeprom, which we're already scavenging for layers
+# just let it control volume everywhere
+ENCODER_MAP_ENABLE = no
+
+AVR_USE_MINIMAL_PRINTF = yes
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+SPACE_CADET_ENABLE = no
+MUSIC_ENABLE = no
+COMBO_ENABLE = no
