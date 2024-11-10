abd09495c9ebd60e368e3c7d0c812fcf415cb9ad
Frank Secilia <frank.secilia@gmail.com>
2024-11-10 06:59:25 -0500

    al68: import patched upstream
    This revision is based on an incomplete patch
    the vendor is working on. My changes are mostly
    trivial: I wrapped a block based on an optional
    feature with a related #ifdef, and I reversed
    the direction of the encoder.

---
+++ keyboards/yunzii/al68/config.h
@@ -0,0 +1,15 @@
+// Copyright 2024 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define WS2812_PWM_DRIVER PWMD4
+#define WS2812_PWM_CHANNEL 3
+#define WS2812_PWM_PAL_MODE 2
+#define WS2812_DMA_STREAM STM32_DMA1_STREAM7
+#define WS2812_DMA_CHANNEL 7
+
+#define FORCE_NKRO
+
+#define USB_SUSPEND_WAKEUP_DELAY 200
+#define USB_WAIT_FOR_ENUMERATION
