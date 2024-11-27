1cef887b45ff6ccf46c2a89c258b5a4e0e567c64
Frank Secilia <frank.secilia@gmail.com>
2024-11-27 17:39:43 -0500

    akko: import from their PR branch
    The 5075[s] claims support for via and qmk. It is
    not yet in the repo, but they have an active
    pull request. This is that pull request's
    keyboards/akko directory.
    
    git remote add akko_5075s_upstream git@github.com:jonylee1986/qmk_firmware_master.git
    git fetch akko_5075s_upstream
    git checkout akko_5075s_upstream/hfd_5075 keyboards/akko/
    git commit

--- keyboards/akko/5075/mcuconf.h
+++ keyboards/akko/5075/mcuconf.h
@@ -13,28 +13,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #pragma once
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
+#include_next <mcuconf.h>
 
-/* SPI Config for spi flash*/
-#define SPI_DRIVER SPIDQ
-#define SPI_SCK_PIN B3
-#define SPI_MOSI_PIN B5
-#define SPI_MISO_PIN B4
-#define SPI_MOSI_PAL_MODE 5
+#undef WB32_SPI_USE_QSPI
+#define WB32_SPI_USE_QSPI TRUE
 
-#define EXTERNAL_FLASH_SPI_SLAVE_SELECT_PIN C12
-
-/* I2C Config for LED Driver */
-#define SNLED27351_I2C_ADDRESS_1 SNLED27351_I2C_ADDRESS_GND
-#define SNLED27351_I2C_ADDRESS_2 SNLED27351_I2C_ADDRESS_VDDIO
-#define I2C1_SCL_PAL_MODE 4
-#define I2C1_OPMODE OPMODE_I2C
-#define I2C1_CLOCK_SPEED 400000 /* 400000 */
-
-#define RGB_TRIGGER_ON_KEYDOWN
+#undef WB32_I2C_USE_I2C1
+#define WB32_I2C_USE_I2C1 TRUE
