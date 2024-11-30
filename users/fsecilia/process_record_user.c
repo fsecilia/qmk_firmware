49219ddb2fd7f4e9de9638d5f6c4ee1c4d1d6af6
Frank Secilia <frank.secilia@gmail.com>
2024-11-30 18:55:38 -0500

    user: untie tap replacement from layer
    The layer is no longer hard coded. It is extracted from the keycode when
    the keycode is a layer tap.

--- users/fsecilia/process_record_user.c
+++ users/fsecilia/process_record_user.c
@@ -1,17 +1,22 @@
-#pragma once
-
 #include "process_record_user.h"
 
-/* hijack LT(0, <kc>) as something we can edit in via and override here, without needing named keys in via */
+bool process_record_layer_tap(uint16_t tap_keycode, keyrecord_t *record) {
+    bool replace_tap = record->event.pressed && record->tap.count;
+    if (!replace_tap) return true;
+
+    switch (tap_keycode) {
+        /* replace backspace tap with ctrl+backspace tap */
+        case KC_BSPC:
+            tap_code16(C(KC_BSPC));
+            return false;
+    }
+
+    return true;
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case LT(0, KC_BKSP):
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(C(keycode));
-                return false;
-            }
-            break;
+    if ((keycode & QK_LAYER_TAP) && !process_record_layer_tap(QK_LAYER_TAP_GET_TAP_KEYCODE(keycode), record)) {
+        return false;
     }
 
     return true;
