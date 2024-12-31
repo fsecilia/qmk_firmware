ef2c386b5a9516b70bfd7bc48278b5acb4d7fe33
Frank Secilia <frank.secilia@gmail.com>
2024-12-31 14:57:05 -0500

    fsecilia/user: move layer tap check into layer tap function
    I had this check in the root process_record_user() because I didn't
    want to pay the cost to call, but this is all lto anyway, so that is
    a silly reason.

--- users/fsecilia/process_record_user.c
+++ users/fsecilia/process_record_user.c
@@ -1,9 +1,10 @@
 #include "process_record_user.h"
 
-bool process_record_layer_tap(uint16_t tap_keycode, keyrecord_t *record) {
-    bool replace_tap = record->event.pressed && record->tap.count;
-    if (!replace_tap) return true;
+bool process_record_layer_tap(uint16_t keycode, keyrecord_t *record) {
+    bool is_tap = (keycode & QK_LAYER_TAP) && record->event.pressed && record->tap.count;
+    if (!is_tap) return true;
 
+    uint16_t tap_keycode = QK_LAYER_TAP_GET_TAP_KEYCODE(keycode);
     switch (tap_keycode) {
         /* replace backspace tap with ctrl+backspace tap */
         case KC_BSPC:
@@ -15,7 +16,7 @@ bool process_record_layer_tap(uint16_t tap_keycode, keyrecord_t *record) {
 }
 
 bool process_record_fsecilia(uint16_t keycode, keyrecord_t *record) {
-    if ((keycode & QK_LAYER_TAP) && !process_record_layer_tap(QK_LAYER_TAP_GET_TAP_KEYCODE(keycode), record)) {
+    if (!process_record_layer_tap(keycode, record)) {
         return false;
     }
 
@@ -23,7 +24,9 @@ bool process_record_fsecilia(uint16_t keycode, keyrecord_t *record) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (!process_record_fsecilia(keycode, record)) return false;
+    if (!process_record_fsecilia(keycode, record)) {
+        return false;
+    }
 
     return true;
 }
