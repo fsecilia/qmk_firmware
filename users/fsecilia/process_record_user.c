5016740b073d8bff34991066562608679256660c
Frank Secilia <frank.secilia@gmail.com>
2025-01-15 18:56:25 -0500

    akko: merge users/fsecilia from fsecilia/combined

---
+++ users/fsecilia/process_record_user.c
@@ -0,0 +1,58 @@
+#include "process_record_user.h"
+#include "lib/getreuer/features/socd_cleaner.h"
+
+bool process_record_socd(uint16_t keycode, keyrecord_t *record) {
+    static socd_cleaner_t cleaners[] = {
+        /* wasd */
+        {{KC_W, KC_S}, SOCD_CLEANER_LAST},
+        {{KC_A, KC_D}, SOCD_CLEANER_LAST},
+    };
+    static int num_cleaners = sizeof(cleaners) / sizeof(*cleaners);
+
+    /* only apply socd on topmost layer */
+    bool on_top_layer = dynamic_keymap_get_layer_count() == layer_switch_get_layer(record->event.key) + 1;
+    if (!on_top_layer) return true;
+
+    for (int cleaner = 0; cleaner < num_cleaners; ++cleaner) {
+        if (!process_socd_cleaner(keycode, record, &cleaners[cleaner])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool process_record_layer_tap(uint16_t keycode, keyrecord_t *record) {
+    bool is_tap = (keycode & QK_LAYER_TAP) && record->event.pressed && record->tap.count;
+    if (!is_tap) return true;
+
+    uint16_t tap_keycode = QK_LAYER_TAP_GET_TAP_KEYCODE(keycode);
+    switch (tap_keycode) {
+        /* replace backspace tap with ctrl+backspace tap */
+        case KC_BSPC:
+            tap_code16(C(KC_BSPC));
+            return false;
+    }
+
+    return true;
+}
+
+bool process_record_fsecilia(uint16_t keycode, keyrecord_t *record) {
+    if (!process_record_layer_tap(keycode, record)) {
+        return false;
+    }
+
+    if (!process_record_socd(keycode, record)) {
+        return false;
+    }
+
+    return true;
+}
+
+__attribute__((weak)) bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!process_record_fsecilia(keycode, record)) {
+        return false;
+    }
+
+    return true;
+}
