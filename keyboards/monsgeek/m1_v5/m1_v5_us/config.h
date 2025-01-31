28cb951c6eb1748d3319746f1a500c6f7268eb4d
Frank Secilia <frank.secilia@gmail.com>
2025-01-31 04:57:03 -0500

    monsgeek/m1v5: fix via encoder

--- keyboards/monsgeek/m1_v5/m1_v5_us/config.h
+++ keyboards/monsgeek/m1_v5/m1_v5_us/config.h
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include "users/fsecilia/config.h"
+
 #define USB_POWER_EN_PIN                    B1 // USB ENABLE pin
 #define LED_POWER_EN_PIN                    A5 // LED ENABLE pin
 #define HS_BAT_CABLE_PIN                    A7 // USB insertion detection pin
